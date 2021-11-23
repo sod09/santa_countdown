5dde855560cc7f9d0dc267305ffdc4b83de84801
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-11-23 13:41:52 +0000

    update

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-const xmasEndDate = new Date ("December 25, 2021 24:00:00").getTime();
+const xmasEndDate = new Date("December 24, 2021 24:00:00").getTime();
 
 console.log(xmasEndDate);
 
 const runFunction = setInterval(() => {
-  
   const today = new Date().getTime();
-  
-  const timeRemainingBeforeXmas = xmasEndDate - today;
 
+  const timeRemainingBeforeXmas = xmasEndDate - today;
 
   const days = Math.floor(timeRemainingBeforeXmas / (1000 * 60 * 60 * 24));
 
-  const hours = Math.floor((timeRemainingBeforeXmas % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (timeRemainingBeforeXmas % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
 
-  const minutes = Math.floor((timeRemainingBeforeXmas % (1000 * 60  * 60 * 60)) / (1000 * 60));
+  const minutes = Math.floor(
+    (timeRemainingBeforeXmas % (1000 * 60 * 60 * 60)) / (1000 * 60)
+  );
 
-  const seconds = Math.floor((timeRemainingBeforeXmas % (1000 * 60 * 60)) / 1000);
+  const seconds = Math.floor(
+    (timeRemainingBeforeXmas % (1000 * 60 * 60)) / 1000
+  );
 
+  const dayDiv = (document.querySelector(".day").innerHTML = `${days} days`);
 
-  const dayDiv = document.querySelector(".day").innerHTML = `${days} days`;
+  const hoursDiv = (document.querySelector(
+    ".hours"
+  ).innerHTML = ` ${hours} hours`);
 
-  const hoursDiv = document.querySelector(".hours").innerHTML = ` ${hours} hours`;
+  const minutesDiv = (document.querySelector(
+    ".minutes"
+  ).innerHTML = ` ${minutes} minutes`);
 
-  const minutesDiv = document.querySelector(".minutes").innerHTML = ` ${minutes} minutes`;
+  const secondsDiv = (document.querySelector(
+    ".seconds"
+  ).innerHTML = `${seconds} seconds`);
 
-  const secondsDiv = document.querySelector(".seconds").innerHTML = `${seconds} seconds`;
-
-  const xmasDiv = document.querySelector(".xmasishere")
+  const xmasDiv = document.querySelector(".xmasishere");
 
   if (timeRemainingBeforeXmas < 0) {
     clearInterval(runFunction);
-    hoursDiv.innerHTML = "" ;
-    dayDiv.innerHTML = "" ;
-    minutesDiv.innerHTML = "" ;
-    secondsDiv.innerHTML = "" ;
-    xmasDiv.innerHTML = `<div class="xmasishere"> FATHER CHRISTMAS IS HERE!</div>`
+    hoursDiv.innerHTML = "";
+    dayDiv.innerHTML = "";
+    minutesDiv.innerHTML = "";
+    secondsDiv.innerHTML = "";
+    xmasDiv.innerHTML = `<div class="xmasishere"> FATHER CHRISTMAS IS HERE!</div>`;
   }
-
-}, 1000)
-
+}, 1000);
 
 // var countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
